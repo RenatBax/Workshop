fcf55fedc3258be8cfadb02d4b4c43bf8ef549ae
RenatBax <renchik_125.111@mail.ru>
2022-01-07 16:08:08 +0300

    Сделана задача 1

--- Task_01_ArrayAandB/Program.cs
+++ Task_01_ArrayAandB/Program.cs
@@ -1,106 +1,104 @@
 ﻿/*
-Cформировать случайным образом целочисленный массив A из натуральных двузначных чисел. 
-Создать на его основе масив B, отбрасывая те, которые
+1 Cформировать случайным образом целочисленный массив A из натуральных двузначных чисел. 
+2 Создать на его основе масив B, отбрасывая те, которые
 
 нарушают порядок возрастания
 больше среднего арифметического элементов A
 чётные
 */
-int[] arrayA = new int[10];
-int t = arrayA.Length;
-int index = 0;
-for(index = 0; index < arrayA.Length; index++)
-{
-    arrayA[index] = new Random().Next(10,100);
-    Console.WriteLine((arrayA[index]) + " ");
-}
-Console.WriteLine();
 
-Console.WriteLine("  Это(и) числа(а) нарушают порядок возрастания: ");
-int ElementB = arrayA[0];
-Console.Write(ElementB);
-for (index = 1; arrayA[index] > ElementB; index++)
+void createArrayA(int[] array)
 {
-    ElementB = arrayA[index];
-    Console.Write(" " +arrayA[index]+ " ");
-}
-Console.WriteLine();
-
-Console.WriteLine("  Cреднее арифметическое число массива А: ");
-double sumArrayA = 0;
-double result;
-for(index = 0; index < t; index++)
-{
-    sumArrayA += arrayA[index];
-    Console.Write(" " +sumArrayA+ " ");
-}
-if(index == t)
-{
-    result = sumArrayA / t;
-    Console.Write(" " + "= " +result+ " ");
-}
-Console.WriteLine();
-
-int[] GetArrayB(int count) //Сделал почти как у Вас, но не работает
-{
-    int index = 0;
-    int[] arrayB = new int[count]; // Наполнение массива В должно происходить
-    //count = ElementB;???         // из количества чисел нарушающих порядок возрастания
-    while(index < 10)
+    for (int index = 0; index < array.Length; index++)
     {
-        arrayB[index] = new Random().Next(10, 100);
-        index++;
+        array[index] = new Random().Next(10, 100);
+    }
+}
+
+void printArray(int[] array)
+{
+    for (int index = 0; index < array.Length; index++)
+    {
+        if (array[index] != 0)
+        Console.Write((array[index]) + " ");
+    }
+}
+
+int arithmeticMeanArrayA(int[] array)
+{
+    int arithmeticMean = 0;
+    int sum = 0;
+    int size = array.Length;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum += array[i];
+        arithmeticMean = sum / size;
+    }
+    return arithmeticMean;
+}
+int[] arrayA = new int[10];
+createArrayA(arrayA);
+Console.Write("Массив А: ");
+printArray(arrayA);
+Console.WriteLine();
+int mean = arithmeticMeanArrayA(arrayA);
+
+int[] createArrayB(int[] array)
+{
+    int[] arrayB = new int[10];
+    int ElementB = array[0];
+    int c = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < ElementB || c > array[i])
+        {
+            ElementB = array[i];
+            continue;
+        }
+        else
+        {
+            c = array[i];
+        }
+        arrayB[i] = array[i];
     }
     return arrayB;
 }
-int[] arrayB = GetArrayB(10);
-//int index = 0;
-while (index < 10)
+
+int[] createArrayBMean(int[] array, int numberMean)
 {
-    int val = arrayB[index];
-    Console.Write(val);
-    index++;
+    int[] arrayB = new int[10];
+    for (int i = 0; i < array.Length; i++)
+    {
+        //if (array[i] < numberMean && array[i] % 2 != 0)
+        if (array[i] < numberMean)
+        {
+            arrayB[i] = array[i];
+        }
+    }
+    return arrayB;
 }
 
-// int GetArrayB(int minValue, int maxValue) Это мой вариант, тоже не работает??????
-// {
-//     return new Random().Next(minValue, maxValue);
-// }
+int[] createArrayBMeanEvent(int[] array)
+{
+    int[] arrayB = new int[10];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+        {
+            arrayB[i] = array[i];
+        }
+    }
+    return arrayB;
+}
 
-// int[] arrayB = new int[ElementB];
-// int indexB = 0;
-// while(indexB < ElementB)
-// {
-//     arrayB[index] = GetArrayB(arrayA[0], ElementB);
-//     indexB++;
-//     Console.Write((arrayB[indexB]) + " ");
-// }
-
-
-// } //Это делал еще вчера, но возник вопрос что нужно будет сравнивать с массивом В, поэтому отложил. На сегодня имена переменных поменялись
-
-// if(result < arrayB)
-// {
-//     arrayB = arrayA[index];
-//     Console.Write(" " +arrayA[index]+ " ");
-// }
-// else
-// {
-//     Console.Write("Число меньше среднего арифметического элементов A ( ");
-// }
-
-// Console.WriteLine();
-// Console.WriteLine("  Четные числа: ");
-// //int result = (Array[])
-// //for (index = 1; arrayA[index] > arrayB; index++)
-// if (arrayA[index]%2==0)
-// {
-//     arrayB = arrayA[index];
-//     Console.Write(" " +arrayA[index]+ " ");
-// }
-// else
-// {
-//     Console.Write("четных чисел нет ( ");
-// }
-// }
-
+int[] arrayB = createArrayB(arrayA);
+int[] arrayBmean = createArrayBMean(arrayB, mean);
+int[] arrayBevent = createArrayBMeanEvent(arrayBmean);
+Console.WriteLine("Массив В, без чисел нарушаюших порядок возрастания: ");
+printArray(arrayB);
+Console.WriteLine();
+Console.WriteLine($"Массив В, без чисел больше среднего арифметического элементов массива A = {mean}");
+printArray(arrayBmean);
+Console.WriteLine();
+Console.WriteLine("Массив В, без четных чисел элементов массива A: ");
+printArray(arrayBevent);
