6f4857904ad3a386cfc8780cf0c16a30098fc126
RenatBax <renchik_125.111@mail.ru>
2022-01-09 14:39:38 +0300

    Кроме знакочередования

--- Task_02ArrayMinMaxValue/Program.cs
+++ Task_02ArrayMinMaxValue/Program.cs
@@ -5,117 +5,81 @@
 // элементы, больше 8
 // знакочередования
 
-int GetArrayA (int minValue, int maxValue)
+int[] createArrayA(int n, int minValue, int maxValue)
 {
-    return new Random().Next(minValue, maxValue);
+    int[] array = new int[n];
+    for (int index = 0; index < array.Length; index++)
+    array[index] = new Random().Next(minValue, maxValue);
+    return array;
 }
-// int[] ArrayA = new int[10]; // Тоже работает
-// int index = 0; 
-// while (index < 10)
-// {
-//     ArrayA[index] = GetArrayA(1, 20);
-//     index++;
-// }
-// void PrintArrayA(int[] ArrayA)
-// {
-//     for(int index = 0; index < 10; index++)
-//     Console.WriteLine(ArrayA[index] + " ");
-// }
-// //int[] arr = CreateArray(10);
-// PrintArrayA(ArrayA);
-
-int[] CreateArray(int n)
+void printArray(int[] array)
 {
-    int[] Array = new int[n];
-    for (int index = 0; index < Array.Length; index++)
-    Array[index] = GetArrayA(-10,20);
-    return Array;
-}
-void PrintArrayA(int[] array)
-{
-    // int position = 0;
-    // while (position < Array.Length)
-    // {
-    //     Console.Write(" " +Array[position]+ " ");
-    //     position++;
-    // }
     for(int index = 0; index < array.Length; index++)
-    Console.WriteLine(array[index] + " ");
-    
-    
+    Console.Write(array[index] + " ");
 }
-// int IndexOff(int[] Array)//, int find Не пригодилось
-// {
-//     int point = Array.Length;
-//     int index = 0;
-//     int position = 0;
-//     while(index < point)
-//     {
-//         if(Array[index] < 0)//== find)
-//         {
-//             position = index;
-//             break;
-//         }
-//         index++;
-//     }
-//     return position;
-//}
-int[] arrA = CreateArray(10);
-PrintArrayA(arrA);
+
+int[] arrayA = createArrayA(10, -5, 15);
+Console.Write("Массив А: ");
+printArray(arrayA);
 Console.WriteLine();
+
+int[] createArrayB(int[]array, int find)
+{
+    int[] arrayB = new int[array.Length];
+    int elementB = array[0];
+    int c = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < elementB)
+        {
+            c = array[i];
+            continue;
+        }
+        else  if (array[i] > elementB || c < array[i])
+        {
+            elementB = array[i];
+        }
+        if (array[i] > find)
+        arrayB[i] = array[i];
+    }
+    return arrayB;
+}
+
 //Элементы массива не нарушающие порядок возрастания
-Console.WriteLine("  Это(и) числа(а) не нарушают порядок возрастания: ");
-int ElementB = arrA[0];
-Console.Write(ElementB);
-int index = 1;
-while (arrA[index] > ElementB)
-{
-    ElementB = arrA[index];
-    Console.Write(" " +arrA[index]+ " ");
-    index++;
-}
-Console.WriteLine();
-//Элементы массива больше восьми
-Console.WriteLine("  Это(и) числа(а) больше восьми: ");
-int Namber = 8;
-for (index = 0; index < 10; index++)
-if(arrA[index] > Namber)
-{
-    Console.Write(" " +arrA[index]+ " ");
-}
-// index = 0; // Работает
-// while (index < 10) //или 10
-// {
-//     if (arrA[index] > Namber)
-//     {
-//         Console.Write(" " +arrA[index]+ " ");
-//     }
-//     index++;
-// }
+Console.WriteLine("Это(и) числа(а) не нарушают порядок возрастания и больше 8: ");
+int[] arrayB = createArrayB(arrayA, 8);
+printArray(arrayB);
+
 Console.WriteLine();
 // Нарушают знакочередование - НЕ СДЕЛАНО
-Console.WriteLine("  Это(и) числа(а) нарушают знакочередование: ЕЩЕ В РАБОТЕ");
-index = 0;
+Console.WriteLine("Это(и) числа(а) нарушают знакочередование: ");
+//int[] posnig = new int[arrayA.Length];
 
-while(arrA[index] < 10)    
+//printArray(posnig);
+for (int index = 0; index < arrayA.Length; index++)
 {
-    if(arrA[0] > 0)
+    if(arrayA[0] < 0)
     {
-        Console.Write(" " + (arrA[index] < 0) + " ");
-        //index++;
+        int j = 0;
+        while (arrayA[j] < 0)
+        Console.Write(arrayA[j] +" ");
+        
+        // arrayA[posit] = arrayA[index];
+        // Console.Write(arrayA[posit] +" ");
+        break;
     }
-    else
-    {
-        if(arrA[0] < 0)    
-        {
-            Console.Write(" " +arrA[index]+ " ");
-            index++;
-        }
-    }
-    index++; 
+    // if (arrayA[0] > 0)
+    // {
+    //     int k = 0;
+    //     while (arrayA[k] > 0)
+    //     Console.Write(arrayA[k] + " ");
+    //     break;
+    // } 
+    //break;
+    //posnig[posit] = arrayA[posit];
+    //return posnig; 
 }
-   
-
+//return posnig[posit];
 
 // int pos = arrA[0];
 
@@ -143,26 +107,3 @@ while(arrA[index] < 10)
     
 
 
-
-
-// Возможно Создал массив В, не проверял??????
-// int[] GetArrayB(int n)
-// {
-//     int index = 0;
-//     int[] arrayB = new int[n]; 
-//     //count = ???         
-//     while(index < n)
-//     {
-//         arrayB[index] = GetArrayA(-10, 10);//new Random().Next(10, 10);
-//         index++;
-//     }
-//     return arrayB;
-// }
-// int[] arrayB = GetArrayB(n);
-// int index = 1;
-// while (index < 10)
-// {
-//     int val = arrayB[index];
-//     Console.Write(" " +val+ " ");
-//     index++;
-// }
